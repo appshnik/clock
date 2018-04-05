d42b74069b4d27b12f70e1df1f9e7d17e692108e
Oleksii Savostianov <appshnik041@gmail.com>
2018-04-05 13:45:31 +0300

    Add src and hdr files

---
+++ include/clib.h
@@ -0,0 +1,55 @@
+#ifndef CLIB
+#define CLIB
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <msp430.h>
+
+/* constants definition */
+#define CPU_F		(20000000UL)		// oscilator frequency
+#define TA_FAC		(CPU_F/1000000UL)	// ticks per microsecond
+#define STR_LEN		(17)			// LCD string length
+#define MAIN_MD		(1)			// MAIN clock mode
+#define SEL_MD		(2)			// select clock mode
+#define C_SET_MD	(4)			// clock SET mode
+#define T_SET_MD	(3)			// timer SET mode
+
+/* IO definition */
+#define SET_B		(1<<0)			// "set" button
+#define UP_B		(1<<1)			// "up" button
+#define DOWN_B		(1<<2)			// "down" button
+#define NEXT_B		(1<<3)			// "next" button
+
+/* event definition */
+#define SET		(~P1IN & SET_B)		// "set" button is pressed
+#define UP		(~P1IN & UP_B)		// "up" button is pressed
+#define DOWN		(~P1IN & DOWN_B)	// "down" button is pressed
+#define NEXT		(~P1IN & NEXT_B)	// "next" button is pressed
+
+/* function macroses */
+#define __delay_ms(ms)	(__delay_cycles((unsigned long)(CPU_F/1000UL * ms)))
+#define __delay_us(us)	(__delay_cycles((unsigned long)(CPU_F/1000000UL * us)))
+//#define __delay_us(us)	__delay_cycles(us/3 + us%3)
+#define __delay_ns(ns)	((ns) <= 50) ? __delay_cycles(1) : __delay_cycles(((ns)/50)+1)
+
+/* function prototipes */
+void init_device(void);
+
+/* Types */
+struct date {
+	int yy;			// year
+	uint8_t mth;		// month
+	uint8_t dd;		// day
+};
+struct time {
+	uint8_t hh;		// hours
+	uint8_t mm;		// minutes
+	uint8_t ss; 		// seconds
+};
+struct dt {
+	struct date date;	// date
+	struct time time;	// time
+};
+
+#endif // CLIB
