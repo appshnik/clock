a88b92eb43736fa2b932096d6182d6ca58633de0
Oleksii Savostianov <appshnik041@gmail.com>
2018-05-03 15:37:47 +0300

    project: add RTC module
    External header and source files is used to communicate with RTC via I2C
    bus. Those files were taken from TI official site.
    
    Current time and date is shown on LCD screens. Timer with alarm signal
    is available.
    
    Signed-off-by: Oleksii Savostianov <appshnik041@gmail.com>

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <board.h>
 #include <lcd.h>
 #include <sb.h>
+#include <rtc.h>
 
 #define _val(var)	((var)>=10)?(""):("0"), (var)
 
@@ -10,6 +11,7 @@
 char top_str[STR_LEN];	/* top string of LCD */
 char bot_str[STR_LEN];	/* bottom string of LCD */
 uint8_t sb_rec_oper;	/* data reading is active */
+
 
 int main(void)
 {
@@ -24,21 +26,41 @@ int main(void)
 	__delay_ms(500UL);
 	lcd_wr_str("Initialization..", 0x40);
 	__delay_ms(500UL);
+
+	rtc_write_ack();
+
 	/* main program loop */
 	while (1) {
 		__delay_ms(500UL);
+
+
 		/* data reading from HT sensor */
 		if (sb_rec_oper) {
 			ht_res = sb_read_data();
 			sb_rec_oper = 0;
 		}
+		/* data reading from RTC */
+		if (!dt_ch) {
+			rtc_get_time();
+			rtc_get_date();
+		}
+		if (timer.state) {
+			rtc_get_timer_st();
+		}
 		/* data writing to RTC */
-		if (dt_ch) {
-			if (dt_ch == 1) {
-				/*TODO: write date/time settings */;
-			}
-			else if (dt_ch == 2) {
-				/* TODO: write timer settings */;
+		if (c_scr == DT_SCR || c_scr == T_SCR) {
+			/* write date/time settings */
+			if (dt_ch & DS_CHANGED)
+				rtc_write_date();
+			else if (dt_ch & TS_CHANGED)
+				rtc_write_time();
+			else if (dt_ch & TMS_CHANGED)
+				rtc_write_timer();
+			else if (dt_ch & AL_ACK)
+				rtc_write_ack();
+			else if (dt_ch & STOP_ALARM) {
+				rtc_write_stop_alarm();
+				timer.state = 0;
 			}
 			dt_ch = 0;
 		}
@@ -63,7 +85,9 @@ int main(void)
 			break;
 		/* date/time screen */
 		case DT_SCR:
-			lcd_wr_str("current time", 0x02);
+			sprintf(top_str, \
+				"current time");
+			lcd_wr_str(top_str, 0x02);
 			sprintf(bot_str, \
 				"%s%d:%s%d:%s%d  %s%d/%s%d", \
 				_val(time.hh), \
@@ -81,9 +105,9 @@ int main(void)
 			lcd_wr_str(top_str, 0x00);
 			sprintf(bot_str, \
 				"%s%d:%s%d:%s%d left", \
-				_val(timer.hh), \
-				_val(timer.mm), \
-				_val(timer.ss));
+				_val(remain.hh), \
+				_val(remain.mm), \
+				_val(remain.ss));
 			lcd_wr_str(bot_str, 0x40);
 			break;
 		/* date/time setup */
@@ -117,6 +141,18 @@ int main(void)
 			lcd_wr_str(bot_str, 0x40);
 			/* display cursor in corresponding position */
 			lcd_wr_instr(_set_ddram(t_curs[c_ind]));
+			break;
+		/* debugging screen */
+		case 1:
+			sprintf(top_str, \
+				"alarm: %d:%d:%d", alarm.hh, alarm.mm, alarm.ss);
+			lcd_wr_str(top_str, 0x00);
+			sprintf(bot_str, \
+				"cur_t: %s%d:%s%d:%s%d", \
+				_val(time.hh), \
+				_val(time.mm), \
+				_val(time.ss));
+			lcd_wr_str(bot_str, 0x40);
 			break;
 		}
 	}
--- src/rtc.c
+++ src/rtc.c
@@ -1,61 +1,206 @@
 #include <common.h>
-/* #include <rtc.h> */
+#include <TI_USCI_I2C_master.h>
+#include <rtc.h>
 
-#define RTC_ADR		(0x68)		/* RTC modules I2C bus address */
+#define RTC_ADDR	(0x68)		/* RTC modules I2C bus address */
+#define RTC_BR_DIV	(250)		/* Divider for baud-rate calculation */
 
-int bcd_nybble_to_int(uint8_t bcd)
-{
-	int value;
-	uint8_t nybble = bcd & 0x0F;
 
-	value = 8 * (nybble & 0x08) + \
-		4 * (nybble & 0x04) + \
-		2 * (nybble & 0x02) + \
-		1 * (nybble & 0x01);
 
-	return value;
-}
+/* Global variables*/
+uint8_t rtc_buf[5];
 
-int bcd_to_int(uint8_t bcd)
+uint8_t rtc_time[] = {0x00};
+uint8_t rtc_date[] = {0x04};
+uint8_t rtc_timer[] = {0x07};
+uint8_t rtc_al_contr[] = {0x0E};
+uint8_t rtc_al_st[] = {0x0F};
+
+uint8_t bcd_to_int(uint8_t bcd)
 {
 	int value;
 	uint8_t msn = (bcd >> 4) & 0x0F;
 	uint8_t lsn = bcd & 0x0F;
 
-	value = 10 * bcd_nybble_to_int(msn) + \
-		bcd_nybble_to_int(lsn);
+	value = 10 * msn + lsn;
 
 	return value;
 }
 
-#if 0
-I2C data write to RTC module:
-	master initiates START -->
-	master sends RTC_ADR + (lsb = 0) -->
-	slave responds -->
-	master sends register (to be written) pointer -->
-	slave responds -->
-	master sends some amount of bytes (register pointer increments
-		automatically) -->
-	master initiates STOP
+uint8_t int_to_bcd(uint8_t val)
+{
+	if (val <= 99)
+		return ((val / 10) << 4) | (val % 10);
+	else
+		return 0;
+}
 
-I2C data read from RTC module:
-	master initiates START -->
-	master sends RTC_ADR + (lsb = 0) -->
-	slave responds -->
-	master sends register (to be read) pointer -->
-	master initiates RESTART -->
-	master sends RTC_ADR + (lsb = 1) -->
-	slave responds -->
-	slave starts to transmit data from register pointer address and
-		further while it receives acknowledgment from master
-		(register pointer is automatically incremented)
-#endif
+/* checks if there is a slave with given address on the bus */
+int rtc_check(uint8_t addr)
+{
+	TI_USCI_I2C_transmitinit(addr, RTC_BR_DIV);
+	while (TI_USCI_I2C_notready());
+	if (TI_USCI_I2C_slave_present(addr))
+		return 1;	/* slave exists */
+	else
+		return 0;	/* slave doesn't exist */
+}
+/* read given amount of bytes b_cnt to rtc_buf
+from specified RTC register rtc_reg*/
+int rtc_read(uint8_t b_cnt, uint8_t *rtc_reg)
+{
+	if (rtc_check(RTC_ADDR)) {
+		/* write register pointer to RTC */
+		TI_USCI_I2C_transmitinit(RTC_ADDR, RTC_BR_DIV);
+		TI_USCI_I2C_transmit(1, rtc_reg);
+		while (TI_USCI_I2C_notready());
 
+		TI_USCI_I2C_receiveinit(RTC_ADDR, RTC_BR_DIV);
+		while (TI_USCI_I2C_notready());
+		TI_USCI_I2C_receive(b_cnt, rtc_buf);
+		while (TI_USCI_I2C_notready());
 
+		return 1;
+	}
+	else
+		return 0;
+}
+/* write given amount of bytes b_cnt from rtc_buf
+to specified RTC register rtc_reg*/
+int rtc_write(uint8_t b_cnt, uint8_t *rtc_reg)
+{
+	rtc_buf[0] = *rtc_reg;
+	if (rtc_check(RTC_ADDR)) {
+		/* write register pointer to RTC */
+		TI_USCI_I2C_transmitinit(RTC_ADDR, RTC_BR_DIV);
+		TI_USCI_I2C_transmit(b_cnt, rtc_buf);
+		while (TI_USCI_I2C_notready());
 
+		return 1;
+	}
+	else
+		return 0;
+}
 
+/* write current time to RTC */
+void rtc_write_time(void)
+{
+	rtc_buf[1] = int_to_bcd(time.ss);
+	rtc_buf[2] = int_to_bcd(time.mm);
+	rtc_buf[3] = int_to_bcd(time.hh);
 
+	rtc_write(4, rtc_time);
+}
 
+/* write current date to RTC */
+void rtc_write_date(void)
+{
+	uint8_t cent = (date.yy >= 2000)?(1):(0);
 
+	rtc_buf[1] = int_to_bcd(date.dd);
+	rtc_buf[2] = int_to_bcd(date.mth) | (uint8_t)((cent)?(0x80):(0x00));
+	rtc_buf[3] = int_to_bcd((date.yy % 100));
 
+	rtc_write(4, rtc_date);
+}
+
+/* write timer settings to RTC */
+void rtc_write_timer(void)
+{
+	uint8_t rem = 0;
+	/* calculate alarm seconds */
+	alarm.ss = (time.ss + timer.ss)%60;
+	rem = (time.ss + timer.ss)/60;
+	/* calculate alarm minutes */
+	alarm.mm = (time.mm + timer.mm + rem)%60;
+	rem = (time.mm + timer.mm + rem)/60;
+	/* calculate alarm hours */
+	alarm.hh = (time.hh + timer.hh + rem)%24;
+
+	rtc_buf[1] = int_to_bcd(alarm.ss);
+	rtc_buf[2] = int_to_bcd(alarm.mm);
+	rtc_buf[3] = int_to_bcd(alarm.hh);
+	rtc_buf[4] = 0x80;
+	/* write alarm time */
+	rtc_write(5, rtc_timer);
+	if (timer.state) {
+		rtc_buf[1] = 0x05;	/* enable alarm OUT */
+		rtc_write(2, rtc_al_contr);
+	}
+}
+/* write alarm acknowledgement to RTC */
+void rtc_write_ack(void)
+{
+	rtc_buf[1] = 0x00;
+
+	rtc_write(2, rtc_al_st);
+}
+/* write alarm switch off command to RTC */
+void rtc_write_stop_alarm(void)
+{
+	rtc_buf[1] = 0x04;
+
+	rtc_write(2, rtc_al_contr);
+}
+/* calculate timer remain */
+void rtc_timer_remain()
+{
+	unsigned long int now_s = time.hh * 360 + time.mm * 60 + time.ss;
+	unsigned long int alm_s = alarm.hh * 360 + alarm.mm * 60 + alarm.ss;
+	unsigned long int day = 86339;
+	unsigned long int rem_s;
+	/* calculate remaining time in seconds */
+	if (now_s > alm_s)
+		rem_s = day - now_s + alm_s;
+	else
+		rem_s = alm_s - now_s;
+
+	remain.ss = rem_s % 60;
+	rem_s = rem_s / 60;
+	remain.mm = rem_s % 60;
+	rem_s = rem_s / 60;
+	remain.hh = rem_s;
+}
+/* get RTC timer state */
+void rtc_get_timer_st(void)
+{
+	if (rtc_read(1, rtc_al_st)) {
+		remain.state = rtc_buf[0] & 0x01;
+	}
+	if (!remain.state) {
+		rtc_timer_remain();
+	}
+}
+/* get current RTC time */
+void rtc_get_time(void)
+{
+	if (rtc_read(3, rtc_time)) {
+		time.hh = bcd_to_int(rtc_buf[2] & 0x3F);
+		time.mm = bcd_to_int(rtc_buf[1]);
+		time.ss = bcd_to_int(rtc_buf[0]);
+	}
+	else {
+		time.hh = 99;
+		time.mm = 99;
+		time.ss = 99;
+	}
+}
+
+/* get current RTC date */
+void rtc_get_date(void)
+{
+	uint8_t bcd_mth = 0;
+	if (rtc_read(3, rtc_date)) {
+		bcd_mth = rtc_buf[1];
+
+		date.mth = bcd_to_int(bcd_mth & 0x1F);
+		date.dd = bcd_to_int(rtc_buf[0]);
+		date.yy = (int)(bcd_to_int(rtc_buf[2])) + \
+			  ((bcd_mth & 0x80)?(2000):(1900));
+	}
+	else {
+		date.yy = 99;
+		date.mth = 99;
+		date.dd = 99;
+	}
+}
